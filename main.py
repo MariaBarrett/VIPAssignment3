a597951f61308890656b4dc1d8983d6b26b24fcf
Alexander Wahl-Rasmussen <a.wahl.r@gmail.com>
2014-01-05 17:13:54 +0100

    System matrix done and math begun.

--- main.py
+++ main.py
@@ -1,57 +1,89 @@
 from __future__ import division
-import pylab
-import scipy
 from scipy.ndimage import filters
-import pylab
-import numpy as np
 from resample import resample
 from splinedraw import *
-import matplotlib.pyplot as plt
 from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 plt.gray() #print everything gray
+plt.ion() #interactive mode
 
 #-------------------------------------------------------------------------
 img1 = np.array(Image.open("images/blacksquare.png"))
 img2 = np.array(Image.open("images/kanizsa_triangle.gif"))
 
 
+"""IniCurveDraw(image,number of points)
+This function takes an image and gives you 5 sec to select some points. 
+From these it draws an interpolated closed spline of num points. 
+It calls Francois' functions to get initial curve and to draw it. 
+The interpolated points are plotted using green circles
+"""
 def IniCurveDraw(im, num):
-	"""
-	This function takes an image and gives you 5 sec to select some points. 
-	From these it draws an interpolated closed spline of num points. 
-	It calls Francois' functions to get initial curve and to draw it. 
-	The interpolated points are plotted using green circles
-	"""
+
 	plt.imshow(im)
 	x, y, points = getInitialCurve(im, num)
-	plt.figure()
 	drawCurve(x,y,im)
-	plt.show()
-
-
-LoG = filters.gaussian_laplace
+	return x, y
 
 
 
-def E1(t,N):
-	g = np.linalg.norm(LoG(t,1.4))**2
-	result = -0.5 * scipy.integrate.quad(g,0,N)
+"""sysmatrix(N,alpha,beta,tao)
+This function takes 5 different inputs, which can be specified by the user.
+It then solves the segmentation by creating a matrix of a linear system.
+It then returns the inverse of said matrix.
+"""
+def sysmatrix(N,alpha,beta,tao):
+	A = tao*beta
+	B = -tao*(alpha+4*beta)
+	C = 1 + tao*(2*alpha+6*beta)
 
-	return result
+	#fill system matrix
+	M = np.zeros((N,N))
+	M[0][0], M[0][1], M[0][2], M[0][-2], M[0][-1] = C, B, A, A, B
+	for p in xrange(1, len(M)):
+	    M[p] = np.roll(M[p-1],1)
+	M = np.linalg.inv(M) 
+
+	return M
 
 
 
+def extenergy(im):
+	iters = 50
 
-def E2(t,N):
-	g = -(LoG(t,1.4))**2
-	result = scipy.integrate.quad(g,0,N)
+	gaus = filters.gaussian_filter()
+	LoG = filters.gaussian_laplace()
 
-	return result
+	sigma = 3
+	fx = np.array(gaus(im,sigma,order=(1,0)))
+	fy = np.array(gaus(im,sigma,order=(0,1)))
+	fxy = np.array(gaus(fx, sigma,order=(0,1)))
+	fxx = np.array(gaus(fx, sigma,order=(1,0)))
+	fyy = np.array(gaus(fy,sigma,order=(0,1)))
+
+	FX = -2*(fx*fxx + fy*fxy)
+	FY = -2*(fx*fxy + fy*fyy)
+	FI = -(LoG(im,sigma))**2
+
+	IX = interp.InterpImage(FX)
+	IY = interp.InterpImage(FY)
+	II = interp.InterpImage(FI)
+
+	return IX,IY,II
+
+def calculate():
+	print "ssomething something"
+
 
 #--------------------------------------------------------------------------
 #Interface
 ##Settings 
+
+
+
 """step-by-step()
 This function allows the user to select whether the segmentation i performed step-by-step or not.
 The  
@@ -131,21 +163,35 @@ def commands(cmd):
 	elif cmd == "1":
 		points = nbr_points()
 		step = step_by_step()
-		print "You have 5 seconds to choose points for the initial curve"
-		IniCurveDraw(img1, points)
-		userinput()
+		print "You have 5 seconds to choose points for the initial curve \n"
+		x = IniCurveDraw(img1, points)
 
 	elif cmd == "2":
 		points = nbr_points()
 		step = step_by_step()
-		print "You have 5 seconds to choose points for the initial curve"
-		IniCurveDraw(img1, points)
-		userinput()
+		print "You have 5 seconds to choose points for the initial curve \n"		
+		x = IniCurveDraw(img2, points)
+		
 
 	elif cmd == "3":
 		print "Quit succesfully."
 		raise SystemExit()
 
+
+	print "Please select the numerical values for alpha, beta and tao seperated by comma"
+	v = raw_input("Alpha,beta,tao: ")
+	v = v.split(',')
+
+	for elem in v:
+		if elem.isdigit() == False:
+			break
+			userinput()
+	
+	alpha,beta,tao = float(v[0]),float(v[1]),float(v[2])
+
+	M = sysmatrix(len(x[1]),alpha,beta,tao)
+
+	userinput()
 
 """main()
 Starts the programme by calling the userinput-function
@@ -156,4 +202,3 @@ def main():
 
 if __name__ =='__main__':
     main(); 
-
